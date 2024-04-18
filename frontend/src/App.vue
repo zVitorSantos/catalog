fc3cd586cd5ea5490fbeb161e02f07cffeb3fd07
zVitorSantos <vitorsantos4736@gmail.com>
2024-04-17 21:54:34 -0300

    fix: simplify the home call

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,14 +3,6 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-  <header>
-    <div class="wrapper">
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-      </nav>
-    </div>
-  </header>
-
   <RouterView />
 </template>
 
