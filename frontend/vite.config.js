920f7b2543154135a20c4b9487a1fdbfdadd6cef
zVitorSantos <vitorsantos4736@gmail.com>
2024-04-17 21:49:23 -0300

    fix: index.html to vite detection, and adding some packages to vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,8 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  root: './src',
-  outputDir: 'public',
+  root: './',
   plugins: [
     vue(),
   ],
