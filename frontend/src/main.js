2cc4a2220e57a89ee5cb9b596492d92b0a89df8c
zVitorSantos <vitorsantos4736@gmail.com>
2024-04-17 21:54:58 -0300

    feat: main.css call

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,9 +1,11 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.use(router)
+import './assets/css/main.css';
 
-app.mount('#app')
+app.use(router);
+
+app.mount('#app');
